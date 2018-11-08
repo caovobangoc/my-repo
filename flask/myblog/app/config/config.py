ced53a2f684d915ba3a795d893de93d864e7ae57
ChauTC <chauta2204@gmail.com>
2018-11-08 09:35:07 +0700

    fix db location

--- flask/myblog/app/config/config.py
+++ flask/myblog/app/config/config.py
@@ -7,7 +7,7 @@ class Config(object):
     
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     #     'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join('/home/chautc/Documents/ChauTC/myblog/app/data', 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir + '/app/data', 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
