e4df3cf006eb882c2a6643e82bc820be63c06e31
ChauTC <chauta2204@gmail.com>
2018-11-09 15:05:59 +0700

    fix config and add .env

--- flask/myblog/app/config/config.py
+++ flask/myblog/app/config/config.py
@@ -1,13 +1,16 @@
 import os
+from os.path import abspath, join, dirname
+from dotenv import load_dotenv
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = abspath(dirname(__file__))
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    #     'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.split(basedir)[0]+'/data', 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
